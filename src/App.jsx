7f47a16917a3e2623150eb3f468d7b54de06aee2
kapade-lokesh <lkkapade@gmail.com>
2025-03-29 17:03:59 +0530

    login/regiter complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Userlayout from "./components/Layout/Userlayout";
 import Home from "./Pages/Home";
 import { Toaster } from "sonner";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 function App() {
   return (
     <>
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Userlayout />}>
             <Route index element={<Home />}></Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import login from "../assets/login.webp";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("user:", { name, email, password });
+  };
+
+
+  return (
+    <div className="flex">
+      <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12">
+        <form onSubmit={handleSubmit} className="w-full max-w-md bg-white p-8 rounded-lg border shadow-sm">
+          <div className="flex justify-center mb-6">
+            <h2 className="text-xl font-medium">E-com</h2>
+          </div>
+
+          <h2 className="text-2xl font-bold text-center mb-6">Hey there !</h2>
+          <p className="text-center mb-6">
+            Enter your username and password to login
+          </p>
+
+          <div className="mb-4">
+            <label htmlFor="" className="block text-sm font-semibold mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              name=""
+              id=""
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className="w-full p-2 border rounded"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="" className="block text-sm font-semibold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              name=""
+              id=""
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              className="w-full p-2 border rounded"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-black text-white rounded-md py-2 hover:bg-gray-800 font-semibold"
+          >
+            Login
+          </button>
+
+          <p className="mt-6 text-center text-sm">
+            Don't have an account ?{" "}
+            <Link to="/register" className="text-blue-500">
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
+
+      <div className="hidden md:block w-1/2  bg-gray-800">
+        <div className="h-full flex flex-col justify-center items-center">
+          <img
+            src={login}
+            alt="login"
+            className="h-[750px] w-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
