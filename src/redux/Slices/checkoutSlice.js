cb677c43fcf2f26c8895ed8eac89a90480926709
kapade-lokesh <lkkapade@gmail.com>
2025-05-14 14:01:04 +0530

    created checkout reducer

---
+++ src/redux/Slices/checkoutSlice.js
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const createChekout = createAsyncThunk(
+  "checkout/create",
+  async (checkoutData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}checkout/create`,
+        checkoutData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      rejectWithValue(error.response.message);
+    }
+  }
+);
+
+const checkoutSlice = createSlice({
+  name: "checkout",
+  innitialState: {
+    checkout: null,
+    loading: null,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createChekout.pending, (state, action) => {
+        (state.loading = true), (state.error = null);
+      })
+      .addCase(createChekout.fulfilled, (state, action) => {
+        state.loading = false;
+        state.checkout = action.payload;
+      })
+      .addCase(createChekout.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      });
+  },
+});
+
+export default checkoutSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./Slices/authSlice";
 import productReducer from "./Slices/productSlice";
 import cartReducers from "./Slices/cartSlice";
+import checkoutReducers from "./Slices/checkoutSlice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
     product: productReducer,
     cart: cartReducers,
+    chekout: checkoutReducers,
   },
 });
 
